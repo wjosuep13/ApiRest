28a13261fbf9acdc26e499121a9614bc75b0e8ea
unknown <201504326@ingenieria.usac.edu.gt>
2018-09-07 18:29:40 -0600

    version3

--- index.php
+++ index.php
@@ -11,18 +11,37 @@ $app = new \Slim\Slim();
 //routing 
 //accediendo VIA URL
 //http:///www.google.com/
-//localhost/apirest/index.php/ => "Hola mundo ...."
+
 $app->get(
     '/',function() use ($app){
     	
-    	//consultas a la base de datos 
-    	// peticiones a otro rest 
-    	// etcetera
+    	
     	$datos = array(
     					"nombre" => "Hola pinche putita", 
     					"edad" => "69"
     					);
-
+						$message = array("message" => "Intentaron Abrir el Porton ALV");
+					$url = 'https://fcm.googleapis.com/fcm/send';
+					$fields = array(
+						//'registration_ids' => $tokens, //tokens
+						//"condition" => "'dogs' in topics || 'cats' in topics",
+						"to" => "Modulo de Garage",
+						'data' => $message
+					);
+					$headers = array('Content-Type: application/json',
+						'Authorization:key=AIzaSyCQFcMTdQEc8tht_CHMsN3AjzYAi5_cbdo'
+					);
+					$ch = curl_init($url);
+					curl_setopt($ch, CURLOPT_POST, true);
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+					$result = curl_exec($ch);
+					if ($result == FALSE)
+						die('Curl failed: ' . curl_error($ch));
+					curl_close($ch);
     	//json 
         echo json_encode($datos);
     }
