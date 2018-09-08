e2982e750c118ff2ef0feff2e4426c7a439876cf
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2018-09-08 10:43:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $app->get(
 						'data' => $message
 					);
 					$headers = array('Content-Type: application/json',
-						'Authorization:key=AIzaSyCQFcMTdQEc8tht_CHMsN3AjzYAi5_cbdo'
+						'Authorization:key=AAAAR2pavnM:APA91bG1Ty5smmk_5k-kcH0TDvCy0zHEHAZ6A9ZeJOCbHAnOx-lm2wA8gf7pFyb09WQHX7BFMXjOhea5JqSJU0VGN7djqOkQ8RDAvjhporK0l77BBS-BM7yJ_1N6cGm3GnJeQtTt5uRM'
 					);
 					$ch = curl_init($url);
 					curl_setopt($ch, CURLOPT_POST, true);
