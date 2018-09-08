91fdab90f8966b76b7deb0c06e6384aa6e3c7a43
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2018-09-08 11:16:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -25,11 +25,11 @@ $app->get(
 					$fields = array(
 						//'registration_ids' => $tokens, //tokens
 						//"condition" => "'dogs' in topics || 'cats' in topics",
-						"to" => "Modulo de Garage",
+						"to" => "/topics/",
 						'data' => $message
 					);
 					$headers = array('Content-Type: application/json',
-						'Authorization:key=1:306727009907:android:c8a61f599fb2ef88'
+						'Authorization:key=AAAAR2pavnM:APA91bG1Ty5smmk_5k-kcH0TDvCy0zHEHAZ6A9ZeJOCbHAnOx-lm2wA8gf7pFyb09WQHX7BFMXjOhea5JqSJU0VGN7djqOkQ8RDAvjhporK0l77BBS-BM7yJ_1N6cGm3GnJeQtTt5uRM'
 					);
 					$ch = curl_init($url);
 					curl_setopt($ch, CURLOPT_POST, true);
