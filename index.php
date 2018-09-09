ad1b561b22c4de08dbf86336993cf9814c7bf609
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2018-09-09 17:56:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ $app = new \Slim\Slim();
 $app->get(
     '/',function() use ($app){
     	
-    	$texto = "{ \"to\" : \"/topics/Arqui1\", \"notification\" : {\"body\" : \"great match!\",\"title\" : \"Portugal vs. Denmark\"\"content_available\" : \"true\",\"priority\" : \"high\", }}"
+    
     	$datos = array(
     					"Title" => "Garage", 
     					"Body" => "Parametro invalido",
 		                        "content_available" => "true",
-                                        "priority" => "high"
+                                        "priority" => "high",
     					);
 	    
 
@@ -27,10 +27,10 @@ $app->get(
 					$fields = array(
 						//'registration_ids' => $tokens, //tokens
 						//"condition" => "'dogs' in topics || 'cats' in topics",
-						"to" => "/topics/Arqui1",
+						"to" => '/topics/Arqui1',
 						"notification" =>$datos
 					);
-	                         echo $texto;
+	                    
 					$headers = array('Content-Type: application/json',
 						'Authorization:key=AAAAR2pavnM:APA91bG1Ty5smmk_5k-kcH0TDvCy0zHEHAZ6A9ZeJOCbHAnOx-lm2wA8gf7pFyb09WQHX7BFMXjOhea5JqSJU0VGN7djqOkQ8RDAvjhporK0l77BBS-BM7yJ_1N6cGm3GnJeQtTt5uRM'
 					);
@@ -40,7 +40,7 @@ $app->get(
 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 					curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-					curl_setopt($ch, CURLOPT_POSTFIELDS, $texto);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
 					$result = curl_exec($ch);
 					
 						echo $result;
