00edc5a4c40ca0e38424e4486dc5bfb3fcedacbb
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2018-09-09 17:34:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,10 @@ $app->get(
     	
     	
     	$datos = array(
-    					"Modulo" => "Garage", 
-    					"Descripcion" => "Parametro invalido"
+    					"Title" => "Garage", 
+    					"Body" => "Parametro invalido"
+		                        "content_available" => "true",
+                                        "priority" => "high"
     					);
 	    
 
@@ -28,6 +30,7 @@ $app->get(
 						"to" => "/topics/Arqui1",
 						"notification" =>$datos
 					);
+	    echo json_encode($fields);
 					$headers = array('Content-Type: application/json',
 						'Authorization:key=AAAAR2pavnM:APA91bG1Ty5smmk_5k-kcH0TDvCy0zHEHAZ6A9ZeJOCbHAnOx-lm2wA8gf7pFyb09WQHX7BFMXjOhea5JqSJU0VGN7djqOkQ8RDAvjhporK0l77BBS-BM7yJ_1N6cGm3GnJeQtTt5uRM'
 					);
