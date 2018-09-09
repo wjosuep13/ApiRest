885a815b37cefa5a2dac8e32d2304b8eafa19473
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2018-09-09 17:38:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $app->get(
 						"to" => "/topics/Arqui1",
 						"notification" =>$datos
 					);
-	                         echo json_encode($fields));
+	                         echo json_encode($fields);
 					$headers = array('Content-Type: application/json',
 						'Authorization:key=AAAAR2pavnM:APA91bG1Ty5smmk_5k-kcH0TDvCy0zHEHAZ6A9ZeJOCbHAnOx-lm2wA8gf7pFyb09WQHX7BFMXjOhea5JqSJU0VGN7djqOkQ8RDAvjhporK0l77BBS-BM7yJ_1N6cGm3GnJeQtTt5uRM'
 					);
