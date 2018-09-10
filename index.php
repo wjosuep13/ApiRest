2015306fce4ec271a73f8bb7bc67330ddfc958cf
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2018-09-09 18:06:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ $app->get(
 					$url = 'https://fcm.googleapis.com/fcm/send';
 					$fields = array(
 						//'registration_ids' => $tokens, //tokens
-						"condition" => "'Arqui1' in topics",
-						//"to" => "/topics/Arqui1"
+						//"condition" => "'Arqui1' in topics",
+						"to" => "/topics/Arqui1"
 						"notification" =>$datos
 					);
 	                    echo json_encode($fields);
